62b5fa825d2eec8f047c29d0822f4624483ccd01
Achim Mertens <achim_mertens@gmx.de>
2023-04-20 08:22:51 +0200

    The problem, that the pictures are not waiting, seems to be solved.

--- surfKibana.js
+++ surfKibana.js
@@ -2,13 +2,45 @@ const { Builder, By, key, until } = require('selenium-webdriver');
 const fs = require('fs');
 const rimraf = require('rimraf');
 //const token = tbd
-const dirPaths = ['Beer', 'Alive', 'LEO'];
+const dirPaths = ['Beer', 'Alive', 'LEO', 'POB', 'SPT', 'Token'];
 const queries =
     [["Beer/01_bought_beer_by_time", 50, "http://raspi:5601/app/kibana#/visualize/edit/bought_beer_by_time?_a=(filters:!(),linked:!f,query:(language:kuery,query:''),uiState:(),vis:(aggs:!((enabled:!t,id:'1',params:(customLabel:'Traded%20$BEER',field:quantity),schema:metric,type:sum),(enabled:!t,id:'2',params:(drop_partials:!f,extended_bounds:(),field:timestamp,interval:auto,min_doc_count:1,scaleMetricValues:!f,timeRange:(from:now-7d,to:now),useNormalizedEsInterval:!t),schema:segment,type:date_histogram),(enabled:!t,id:'3',params:(customLabel:Buyer,field:buyer.keyword,missingBucket:!f,missingBucketLabel:Missing,order:desc,orderBy:'1',otherBucket:!f,otherBucketLabel:Other,size:5),schema:group,type:terms)),params:(addLegend:!t,addTimeMarker:!f,addTooltip:!t,categoryAxes:!((id:CategoryAxis-1,labels:(filter:!t,show:!t,truncate:100),position:bottom,scale:(type:linear),show:!t,style:(),title:(),type:category)),grid:(categoryLines:!f),labels:(show:!f),legendPosition:right,seriesParams:!((data:(id:'1',label:'Traded%20$BEER'),drawLinesBetweenPoints:!t,lineWidth:2,mode:stacked,show:!t,showCircles:!t,type:histogram,valueAxis:ValueAxis-1)),thresholdLine:(color:%23E7664C,show:!f,style:full,value:10,width:1),times:!(),type:histogram,valueAxes:!((id:ValueAxis-1,labels:(filter:!f,rotate:0,show:!t,truncate:100),name:LeftAxis-1,position:left,scale:(mode:normal,type:linear),show:!t,style:(),title:(text:'Traded%20$BEER'),type:value))),title:'Bought%20$BEER%20By%20Time',type:histogram))&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-7d,to:now))&embed=true"],
     ["Beer/02_TopTenBeerBuyers", 50, "http://raspi:5601/goto/acbf5274af8dd0f0cb245476b3470f87"],
     ["Beer/03_CommulatedAmountOfBoughtBeer", 50, "http://raspi:5601/app/kibana#/visualize/edit/commulated_amount_of_bought_beer?embed=true&embed=true&_a=(filters:!(),linked:!f,query:(language:kuery,query:''),uiState:(),vis:(aggs:!((enabled:!t,id:'1',params:(customLabel:'$BEER',field:quantity),schema:metric,type:sum),(enabled:!t,id:'2',params:(customLabel:'$HIVE',field:volume),schema:metric,type:sum),(enabled:!t,id:'3',params:(customLabel:Buyer,field:buyer.keyword,missingBucket:!f,missingBucketLabel:Missing,order:desc,orderBy:'2',otherBucket:!f,otherBucketLabel:Other,size:10),schema:segment,type:terms)),params:(addLegend:!t,addTimeMarker:!f,addTooltip:!t,categoryAxes:!((id:CategoryAxis-1,labels:(filter:!t,rotate:75,show:!t,truncate:100),position:bottom,scale:(type:linear),show:!t,style:(),title:(),type:category)),grid:(categoryLines:!f,valueAxis:''),labels:(show:!f),legendPosition:right,seriesParams:!((data:(id:'1',label:'$BEER'),drawLinesBetweenPoints:!t,lineWidth:2,mode:normal,show:!t,showCircles:!t,type:histogram,valueAxis:ValueAxis-1),(data:(id:'2',label:'$HIVE'),drawLinesBetweenPoints:!t,lineWidth:2,mode:stacked,show:!t,showCircles:!t,type:histogram,valueAxis:ValueAxis-1)),thresholdLine:(color:%23E7664C,show:!f,style:full,value:10,width:1),times:!(),type:histogram,valueAxes:!((id:ValueAxis-1,labels:(filter:!f,rotate:0,show:!t,truncate:100),name:LeftAxis-1,position:left,scale:(mode:normal,type:linear),show:!t,style:(),title:(text:'Commulated%20Amount%20Of%20Bought%20$BEER%20And%20sold%20$HIVE'),type:value))),title:'Commulated%20Amount%20Of%20Bought%20$BEER%20Per%20Person',type:histogram))&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-7d,to:now))"],
-    ["Beer/04_CommulatedAmountOfSoldBeer", 50, "http://raspi:5601/app/kibana#/visualize/edit/commulated_amount_of_sold_beer?embed=true&embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-7d,to:now))&_a=(filters:!(),linked:!f,query:(language:kuery,query:''),uiState:(),vis:(aggs:!((enabled:!t,id:'1',params:(customLabel:'$BEER',field:quantity),schema:metric,type:sum),(enabled:!t,id:'3',params:(customLabel:'$HIVE',field:volume),schema:metric,type:sum),(enabled:!t,id:'4',params:(customLabel:Seller,field:seller.keyword,missingBucket:!f,missingBucketLabel:Missing,order:desc,orderBy:'3',otherBucket:!f,otherBucketLabel:Other,size:10),schema:segment,type:terms)),params:(addLegend:!t,addTimeMarker:!f,addTooltip:!t,categoryAxes:!((id:CategoryAxis-1,labels:(filter:!f,rotate:75,show:!t,truncate:100),position:bottom,scale:(type:linear),show:!t,style:(),title:(),type:category)),grid:(categoryLines:!f),labels:(show:!f),legendPosition:right,orderBucketsBySum:!f,row:!f,seriesParams:!((data:(id:'1',label:'$BEER'),drawLinesBetweenPoints:!t,lineWidth:2,mode:normal,show:!t,showCircles:!t,type:histogram,valueAxis:ValueAxis-1),(data:(id:'3',label:'$HIVE'),drawLinesBetweenPoints:!t,lineWidth:2,mode:stacked,show:!t,showCircles:!t,type:histogram,valueAxis:ValueAxis-1)),thresholdLine:(color:%23E7664C,show:!f,style:full,value:10,width:1),times:!(),type:histogram,valueAxes:!((id:ValueAxis-1,labels:(filter:!f,rotate:0,show:!t,truncate:100),name:LeftAxis-1,position:left,scale:(defaultYExtents:!f,mode:normal,type:linear),show:!t,style:(),title:(text:'Commulated%20Amount%20Of%20Sold%20$BEER%20And%20Earned%20$HIVE'),type:value))),title:'Commulated%20Amount%20Of%20Sold%20$BEER%20Per%20Person',type:histogram))"]
-    ];
+    ["Beer/04_CommulatedAmountOfSoldBeer", 50, "http://raspi:5601/app/kibana#/visualize/edit/commulated_amount_of_sold_beer?embed=true&embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-7d,to:now))&_a=(filters:!(),linked:!f,query:(language:kuery,query:''),uiState:(),vis:(aggs:!((enabled:!t,id:'1',params:(customLabel:'$BEER',field:quantity),schema:metric,type:sum),(enabled:!t,id:'3',params:(customLabel:'$HIVE',field:volume),schema:metric,type:sum),(enabled:!t,id:'4',params:(customLabel:Seller,field:seller.keyword,missingBucket:!f,missingBucketLabel:Missing,order:desc,orderBy:'3',otherBucket:!f,otherBucketLabel:Other,size:10),schema:segment,type:terms)),params:(addLegend:!t,addTimeMarker:!f,addTooltip:!t,categoryAxes:!((id:CategoryAxis-1,labels:(filter:!f,rotate:75,show:!t,truncate:100),position:bottom,scale:(type:linear),show:!t,style:(),title:(),type:category)),grid:(categoryLines:!f),labels:(show:!f),legendPosition:right,orderBucketsBySum:!f,row:!f,seriesParams:!((data:(id:'1',label:'$BEER'),drawLinesBetweenPoints:!t,lineWidth:2,mode:normal,show:!t,showCircles:!t,type:histogram,valueAxis:ValueAxis-1),(data:(id:'3',label:'$HIVE'),drawLinesBetweenPoints:!t,lineWidth:2,mode:stacked,show:!t,showCircles:!t,type:histogram,valueAxis:ValueAxis-1)),thresholdLine:(color:%23E7664C,show:!f,style:full,value:10,width:1),times:!(),type:histogram,valueAxes:!((id:ValueAxis-1,labels:(filter:!f,rotate:0,show:!t,truncate:100),name:LeftAxis-1,position:left,scale:(defaultYExtents:!f,mode:normal,type:linear),show:!t,style:(),title:(text:'Commulated%20Amount%20Of%20Sold%20$BEER%20And%20Earned%20$HIVE'),type:value))),title:'Commulated%20Amount%20Of%20Sold%20$BEER%20Per%20Person',type:histogram))"],
+    ["Beer/05_PriceOfBeer", 10, "http://raspi:5601/app/kibana#/visualize/edit/price_of_beer?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["Beer/06_TableOfBeerBuyVsSellRequest", 10, "http://raspi:5601/app/kibana#/visualize/edit/e6b0e8b0-09a9-11ed-b472-c7a41b33ae44?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["LEO/01_BoughtLeoByTime", 10, "http://raspi:5601/app/kibana#/visualize/edit/bought_leo_by_time?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["LEO/02_TopTenLeoBuyers", 10, "http://raspi:5601/app/kibana#/visualize/edit/top_ten_leo_buyers?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["LEO/03_CommulatedAmountOfBoughtLeo", 10, "http://raspi:5601/app/kibana#/visualize/edit/commulated_amount_of_bought_leo?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["LEO/04_CommulatedAmountOfSoldLeo", 10, "http://raspi:5601/app/kibana#/visualize/edit/commulated_amount_of_sold_leo?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["LEO/05_PriceOfLeo", 10, "http://raspi:5601/app/kibana#/visualize/edit/price_of_leo?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["LEO/06_TableOfLeoBuyVsSellRequest", 10, "http://raspi:5601/app/kibana#/visualize/edit/6046b910-0aec-11ec-9586-edd1781b885c?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    
+    ["POB/01_BoughtPobByTime",10,"http://raspi:5601/app/kibana#/visualize/edit/bought_pob_by_time?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["POB/02_TopTenPobBuyers", 10, "http://raspi:5601/app/kibana#/visualize/edit/top_ten_pob_buyers?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["POB/03_CommulatedAmountOfBoughtPob", 10, "http://raspi:5601/app/kibana#/visualize/edit/commulated_amount_of_bought_pob?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["POB/04_CommulatedAmountOfSoldPob", 10, "http://raspi:5601/app/kibana#/visualize/edit/commulated_amount_of_sold_pob?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["POB/05_PriceOfPob", 10, "http://raspi:5601/app/kibana#/visualize/edit/price_of_pob?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["POB/06_TableOfPobBuyVsSellRequest", 10, "http://raspi:5601/app/kibana#/visualize/edit/808576c0-0aed-11ec-9586-edd1781b885c?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    
+    ["SPT/01_BoughtSptByTime",10,"http://raspi:5601/app/kibana#/visualize/edit/bought_spt_by_time?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["SPT/02_TopTenSptBuyers", 10, "http://raspi:5601/app/kibana#/visualize/edit/top_ten_spt_buyers?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["SPT/03_CommulatedAmountOfBoughtSpt", 10, "http://raspi:5601/app/kibana#/visualize/edit/commulated_amount_of_bought_spt?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["SPT/04_CommulatedAmountOfSoldSpt", 10, "http://raspi:5601/app/kibana#/visualize/edit/commulated_amount_of_sold_spt?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["SPT/05_PriceOfSpt", 10, "http://raspi:5601/app/kibana#/visualize/edit/price_of_spt?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["SPT/06_TableOfSptBuyVsSellRequest", 10, "http://raspi:5601/app/kibana#/visualize/edit/a2444c20-0aeb-11ec-9586-edd1781b885c?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    
+    ["Alive/01_BoughtALiveByTime",10,"http://raspi:5601/app/kibana#/visualize/edit/bought_alive_by_time?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["Alive/02_TopTenAliveBuyers", 10, "http://raspi:5601/app/kibana#/visualize/edit/top_ten_alive_buyers?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["Alive/03_CommulatedAmountOfBoughtAlive", 10, "http://raspi:5601/app/kibana#/visualize/edit/commulated_amount_of_bought_alive?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["Alive/04_CommulatedAmountOfSoldAlive", 10, "http://raspi:5601/app/kibana#/visualize/edit/commulated_amount_of_sold_alive?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["Alive/05_PriceOfAlive", 10, "http://raspi:5601/app/kibana#/visualize/edit/price_of_alive?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    
+    ["Token/01_HivePerToken",10,"http://raspi:5601/app/kibana#/visualize/edit/618c5150-1b67-11ec-8610-27c40f2d50c0?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["Token/02_USDPerToken",10,"http://raspi:5601/app/kibana#/visualize/edit/9c1c8920-1b67-11ec-8610-27c40f2d50c0?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],
+    ["Token/03_TableOfTokenPrices",10,"http://raspi:5601/app/kibana#/visualize/edit/8bf79f60-1b69-11ec-8610-27c40f2d50c0?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))"],    
+];
 let driver;
 
 
@@ -32,63 +64,18 @@ async function runallFunctions() {
             const duration = query[1];
             const url = query[2];
             console.log("filename = ", filename, " url = ", url);
-            //await takeSshot(filename, duration, url);
-
-            
             await driver.get(url);
             await driver.wait(until.elementLocated(By.className("euiIcon euiIcon--large euiIcon-isLoaded")), duration * 1000);
             await driver.manage().window().setRect({ width: 1024, height: 768 });
-            await new Promise(resolve => setTimeout(resolve, 1000));  // be sure, that the folders are deleted before creating new ones
-            await driver.takeScreenshot().then(
-                function (image) {
-                    fs.writeFile('./screenshots/' + filename + '.png', image, 'base64', function (err) {
-                        if (err) console.log(err);
-                    });
-                }
-            );
+            await new Promise(resolve => setTimeout(resolve, 1500));  // be sure, that everything is loaded
+            const image = await driver.takeScreenshot();
+            await fs.promises.writeFile('./screenshots/' + filename + '.png', image, 'base64');            
         }
         await driver.quit();
-
-    } catch (err) {
-        console.error(`Fehler `, err);
-    }
-
-}
-
-async function takeSshot(filename, duration, url) {
-
-    try {
-        driver = await new Builder().forBrowser('chrome').build();
-        await driver.get(url);
-        await driver.wait(until.elementLocated(By.className("euiIcon euiIcon--large euiIcon-isLoaded")), duration * 1000);
-        await driver.manage().window().setRect({ width: 1024, height: 768 });
-        await new Promise(resolve => setTimeout(resolve, 1000));  // be sure, that the folders are deleted before creating new ones
-        await driver.takeScreenshot().then(
-            function (image) {
-                fs.writeFile('./screenshots/' + filename + '.png', image, 'base64', function (err) {
-                    if (err) console.log(err);
-                });
-            }
-        );
-        await driver.quit();
     } catch (err) {
         console.error(`Fehler `, err);
     }
 }
-
-//      await driver.get("http://raspi:5601/goto/acbf5274af8dd0f0cb245476b3470f87");
-//          await driver.wait(until.elementLocated(By.className("euiIcon euiIcon--large euiIcon-isLoaded")), duration*1000);
-//          await driver.manage().window().setRect({ width: 1024, height: 768 });
-//          await new Promise(resolve => setTimeout(resolve, 3000));
-//                await driver.takeScreenshot().then(
-//                        function(image) {
-//                            fs.writeFile('./screenshots/'+"Beer/02_TopTenBeerBuyers"+'.png', image, 'base64', function(err) {
-//                                if (err) console.log(err);
-//                            });
-//                        }
-//                    );
-//    await driver.quit();
-
 
 async function deleteDirs() {
     for (const dirPath of dirPaths) {
