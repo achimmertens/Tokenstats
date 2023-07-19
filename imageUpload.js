dea7aead4f72df1f578e2b0501f79ce84bb71a85
Achim Mertens <achim_mertens@gmx.de>
2023-07-19 07:51:07 +0200

    API-URL wird auch zurückgegeben

--- imageUpload.js
+++ imageUpload.js
@@ -1,25 +1,41 @@
-const { authHeader } = require('./config'); // Pfade entsprechend anpassen
+const { authHeader } = require('./config'); 
 
 async function getAuthorizationToken() {
   const url = 'https://api.backblazeb2.com/b2api/v2/b2_authorize_account';
-
   try {
     const response = await fetch(url, {
       headers: {
         'Authorization': authHeader,
       },
     });
-
     if (!response.ok) {
       throw new Error('Fehler bei der Anfrage: ' + response.status);
     }
-
     const data = await response.json();
     console.log("data = ", data);
-    // Extrahiere den Autorisierungs-Token aus der Antwort
     const authToken = data.authorizationToken;
+    const apiUrl = data.apiUrl
+    return {authToken, apiUrl};
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
+}
 
-    // Gib den Autorisierungs-Token zurück
+async function getUploadURL() {
+  const url = 'https://api005.backblazeb2.com/b2api/v2/b2_get_upload_url';
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'Authorization': authHeader,
+      },
+    });
+    if (!response.ok) {
+      throw new Error('Fehler bei der Anfrage: ' + response.status);
+    }
+    const data = await response.json();
+    console.log("data = ", data);
+    const authToken = data.authorizationToken;
     return authToken;
   } catch (error) {
     console.error(error.message);
@@ -29,8 +45,15 @@ async function getAuthorizationToken() {
 
 // Funktion aufrufen und das Ergebnis in eine Variable laden
 async function main() {
-  const authToken = await getAuthorizationToken();
-  console.log('Authorization-Token:', authToken);
+  const result = await getAuthorizationToken();
+  
+  if (result) {
+    const { authToken, apiUrl } = result;
+    console.log('Authorization-Token:', authToken);
+    console.log('API-URL:', apiUrl);
+  } else {
+    console.log('Fehler beim Abrufen des Authorization-Tokens');
+  }
 }
 
 main();
