3c78157da584f7c1c4de8ce341f972f76b05372c
Achim Mertens <achim_mertens@gmx.de>
2023-09-12 15:14:55 +0200

    chore: repairs postToHive.js

--- postToSteem.js
+++ postToSteem.js
@@ -1,34 +1,50 @@
 const fs = require('fs');
-const hive = require('@hiveio/hive-js');
 const steem = require('steem');
 const config = require('./steemConfig.js');
 
 const privateKey = config.privateKey;
-//hive.api.setOptions({ url: 'https://api.hive.blog' });
-hive.api.setOptions({ url: 'https://api.steem.blog' });
-
 const parentAuthor = ''; // Leer lassen, da es sich um einen eigenständigen Post handelt
-const parentPermlink = 'ein-test-von-achimmertens04'; // Permlink des Elternbeitrags, kann frei gewählt werden
+const parentPermlink = 'hive-121566'; // Permlink des Elternbeitrags, kann frei gewählt werden
 const author = 'achimmertens';
 const permlink = new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
-const title = 'Eine Test von achimmertens';
-const bodyFilePath = 'screenshots_2023-09-01/\BEER/\BEERText.txt';
+const title = 'Ein Test von achimmertens';
+const bodyFilePath = 'test.md';
+
 // Beneficiaries-Objekt erstellen
-const beneficiaries = [{ account: 'jaraumoses', weight: 10000 }];
+const beneficiaries = [{ account: 'jaraumoses', weight: 5000 }];
 
 // Den Inhalt der body.md-Datei lesen
 const body = fs.readFileSync(bodyFilePath, 'utf-8');
 
-//hive.broadcast.comment(
-steem.broadcast.comment(  
-  privateKey,
-  parentAuthor,
-  parentPermlink,
-  author,
-  permlink,
-  title,
-  body,
-  { tags: ['test'], app: 'test/0.1'  },
+// Konfiguration der STEEM-API
+steem.api.setOptions({ url: 'https://api.steemit.com' });
+
+// Erstellen des Posts mit Beneficiaries
+steem.broadcast.send(
+  {
+    operations: [
+      ['comment', {
+        parent_author: parentAuthor,
+        parent_permlink: parentPermlink,
+        author: author,
+        permlink: permlink,
+        title: title,
+        body: body,
+        json_metadata: JSON.stringify({ tags: ['test'], app: 'test/0.1' }),
+      }],
+      ['comment_options', {
+        author: author,
+        permlink: permlink,
+        allow_votes: true,
+        allow_curation_rewards: true,
+        max_accepted_payout: '1000000.000 SBD', // Setzen Sie das Asset-Symbol auf SBD oder ein anderes gültiges Asset-Symbol
+        percent_hbd: 10000,
+        percent_steem_dollars: 0,
+        extensions: [[0, { beneficiaries }]],
+      }]
+    ]
+  },
+  { posting: privateKey },
   function(err, result) {
     if (err) {
       console.error(err);
--- test.md
+++ test.md
@@ -1,5 +1,29 @@
-Hello,
+Am Montag, dem 11.9.2023, war ich Teilnehmer einer philosophischen Diskussion mit Markus Melchers in Eschweiler.
+Wie immer war es ein lebhaftes Gespräch mit ca. 20 Teilnehmern, diesmal zu Thema: "Reichtum und Armut".
+Hier meine Skizzen:
 
-this is a test. Please ignore everything here and don't upvote this post!
 
-Regards, Achim
+![20230912_120113~2.jpg](https://files.peakd.com/file/peakd-hive/achimmertens/23tHZgLUJqHJa4ghQ7oo8YYz4jz4mmgUVoMkFSyrJmCnku5EHtknHj7gHQzdU3sAbSfuX.jpg)
+
+![20230912_120128~2.jpg](https://files.peakd.com/file/peakd-hive/achimmertens/EokiUzacuuuE82pnkRjByqWxCCnp2TNdvq7a6tQ6giyL1FeKLHXX42hvK4nV7D9MfFD.jpg)
+
+![20230912_120143~2.jpg](https://files.peakd.com/file/peakd-hive/achimmertens/23uRJ2RdQgUywioHxQHj7XUFs5LeLnTtdTastvdrDkU1bt46ZpKAGdWW6AQpvYPRdmTCV.jpg)
+
+![20230912_120156~2.jpg](https://files.peakd.com/file/peakd-hive/achimmertens/23tw5fSkrmuxYLbc4RLnx9dpG1b2mBG8veLWiPKmdTeAPaLSpHoqWGcPBHjyB7TvuwS2j.jpg)
+
+Die Kernaussagen sind, dass Reichtum an sich nicht böse ist (solange es nicht so viel Geld ist, um damit die Demokratie zu gefährden). Reichtum braucht Verantwortung. Es gibt Gute und Böse Reiche. Ohne Reiche haben die Armen auch ein Problem (kein Invest), aber Trickle Down funktioniert nicht.
+
+Die wichtigste Botschaft ist, dass ungezügelter Reichtum die Gesellschaft spaltet und die Demokratie gefährdet (siehe USA). Um das zu verhindern fordern selbst einige Reiche verschiedene Maßnahmen (z.B. Erbschafts- und andere Steuern).
+
+Nächste Diskussionen:
+- 16.10.2023 – Was ist Geld?
+- 11.12.2023 – Wirtschaft und Moral
+
+Links: 
+- https://greensniper.wordpress.com
+- https://peakd.com/@achimmertens
+- http://sinn-auf-raedern.de
+
+
+
+Gruß, Achim
